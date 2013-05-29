e1adf1d814faa5a3f5d307ea71ae1b6f164a0c25
Kenji <kenji0x02@gmail.com>
2013-05-30 01:31:33 +0900

    番組詳細情報を表示

--- public/javascripts/marico.js
+++ public/javascripts/marico.js
@@ -1,3 +1,28 @@
+// yyyyMMddHHmmを時間に変換
+// javascriptにはstrftimeがない、、、
+String.prototype.toTime = function() {
+  var fullYear = this.substr(0, 4) - 0;
+  var month = this.substr(4, 2) - 1;
+  var date = this.substr(6, 2) - 0;
+  var hours = this.substr(8, 2) - 0;
+  var minutes = this.substr(10, 2) - 0;
+  var second = 0;
+  var time = new Date(fullYear, month, date, hours, minutes, second);
+  console.log(time);
+  return time;
+};
+
+// 経過時間をHH:mmで出力
+function progressTime(startTime, endTime) {
+  // 割り切れない場合は切り捨て
+  var progressTimeTotalMinute = Math.floor((endTime.getTime() - startTime.getTime()) / 60000);
+  var progressTimeMinute = progressTimeTotalMinute % 60;
+  var progressTimeHour = (progressTimeTotalMinute - progressTimeMinute) / 60;
+  if (progressTimeMinute < 10) {
+    progressTimeMinute = "0" + progressTimeMinute;
+  }
+  return progressTimeHour + ":" + progressTimeMinute;
+}
 // クリック音を鳴らす
 function playSound() {
   document.getElementById("playSound").play();
@@ -46,6 +71,12 @@ socket.on('message', function(message) {
     var beforeProgramInfo = $(domIDSpan).text();
     var beforeProgramInfoWidth = $(domIDSpan).width();
 
+    // tag設定
+    $(domID).attr('data-title', programLists[station]['title']);
+    $(domID).attr('data-start-time', programLists[station]['startTime']);
+    $(domID).attr('data-end-time', programLists[station]['endTime']);
+    $(domID).attr('data-content', programLists[station]['content']);
+
     // 番組情報表示テキスト作成
     var iterateCnt = 3;
     var spacer = "　　";
@@ -54,7 +85,7 @@ socket.on('message', function(message) {
     if(beforeProgramInfo.length == 0) spacer = "";
 
     for(var i = 0; i < iterateCnt; i++) {
-      displayInfo = displayInfo + spacer + programLists[station];
+      displayInfo = displayInfo + spacer + programLists[station]['title'];
       // 全角スペース２文字で間隔を空けて表示
       spacer = "　　";
     }
@@ -84,7 +115,7 @@ socket.on('message', function(message) {
       marquee: $(domIDSpan)
       , startPosition: startPosition
       , endPosition: endPosition
-      , callbackText: programLists[station]
+      , callbackText: programLists[station]['title']
       , callbackLeft: offsetPosition
     }
     $(domID).marquee(marqueeOpt);
@@ -102,3 +133,39 @@ function commandEmitWithRemocon(specifiedRemoconName, action){
   socket.emit('commandEmit', command);
   console.log("command emit");
 }
+
+// チャンネルボタンが押されたとき
+$('.marquee').click(function(){
+  // 閉じているときだけ開く
+  if (document.getElementById("program-info").clientHeight == 0) {
+    $('#program-info').collapse('show');
+  }
+  var id = $(this).attr('id');
+  console.log(id);
+  // 属性更新
+  var title = $(this).attr('data-title');
+  $("#program-info h4").text(title);
+  var content = $(this).attr('data-content');
+  $("#program-info p").text(content);
+  // var startTimeHour = $(this).attr('data-start-time').substr(8, 2);
+  // var startTimeMinutes = $(this).attr('data-start-time').substr(10, 2);
+  // var displayStartTime = startTimeHour + ':' + startTimeMinutes;
+  // $("#program-info span.start-time").text(displayStartTime);
+  // var endTimeHour = $(this).attr('data-end-time').substr(8, 2);
+  // var endTimeMinutes = $(this).attr('data-end-time').substr(10, 2);
+  // var displayEndTime = endTimeHour + ':' + endTimeMinutes;
+  // $("#program-info span.end-time").text(displayEndTime);
+  // 放送進捗率
+  var startTime = ($(this).attr('data-start-time')).toTime();
+  var endTime = ($(this).attr('data-end-time')).toTime();
+  var nowTime = new Date();
+  var percentage = (nowTime.getTime() - startTime.getTime()) / (endTime.getTime() - startTime.getTime());
+  percentage = parseInt(percentage * 100);
+  var style = 'width: ' + percentage + '%;';
+  $('#program-info .bar').attr('style', style);
+  // 番組トータル時間と経過時間(YouTube風)
+  var totalTime = progressTime(startTime, endTime);
+  var nowProgressTime = progressTime(startTime, nowTime);
+  $("#program-info span.start-time").text(nowProgressTime);
+  $("#program-info span.end-time").text(totalTime);
+});
